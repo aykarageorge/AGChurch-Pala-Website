57170e6ae29411b44db5ba8273f5dc2d462d1bdc
aykarageorge <gaykara@xactlycorp.com>
2023-07-29 20:56:16 +0530

    Full website push

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(function() {
+    $('.ministry_image').on('click', function() {
+        $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+        $('#imagemodal').modal('show');   
+    });		
+});
