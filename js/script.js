2933c5b3992b27c99600d8fe0dacddf49b28b455
aykarageorge <aykarageorge@gmail.com>
2023-08-05 22:54:56 +0530

    final reCAPTCHA push

--- js/script.js
+++ js/script.js
@@ -27,34 +27,6 @@ const siteKey = "6LevfoInAAAAAOvAyN_eLNlGG0sQxBlxJZXFwFJ5";
   
 const url = 'https://script.google.com/macros/s/AKfycbyOi1EM7FqpsPRErVPYN9oQtjFBsDx4g7VdVG3AlVOKR0zNXDm-mWm-2FLsWv5GNgHBzQ/exec';
 
-// let loginForm = document.getElementById("prayerForm");
-
-// loginForm.addEventListener("submit", (e) => {
-//     e.preventDefault();
-
-//     data.gCaptchaResponse = document.getElementById('g-recaptcha-response').value;
-//     data.name = document.getElementById("name");
-//     data.phone = document.getElementById("phone");
-//     data.email = document.getElementById("email");
-//     data.message = document.getElementById("message");
-    
-//     fetch(url, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'text/plain;charset=utf-8',
-//             'Access-Control-Allow-Origin': '*',
-//         },
-//         body: JSON.stringify(data),
-//     })
-//     .then((res) => res.json())
-//     .then((data) => console.log('data', data))
-//     .catch((err) => console.log('err', err));
-// });
-
-
-// Prayer Request submit form
-
- // Function to handle form submission
  function handleSubmit(event) {
     event.preventDefault();
 
@@ -85,3 +57,4 @@ const url = 'https://script.google.com/macros/s/AKfycbyOi1EM7FqpsPRErVPYN9oQtjFB
   }
 
   document.getElementById("prayerForm").addEventListener("submit", handleSubmit);
+  // Prayer Request submit form
